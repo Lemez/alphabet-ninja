e6d35e4c22e2a7f334bc869104c0d4f5f1f25924
Jonathan Walton <lemez9@gmail.com>
2014-07-14 20:52:14 +0300

    fixed small hebrew bugs

--- js/keysynth_copy.js
+++ js/keysynth_copy.js
@@ -15,7 +15,8 @@ $(document).ready(function () {
 	var KEYCODETOCHAR = {
 		// 8:"Backspace",9:"Tab",13:"Enter",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause/Break",20:"Caps Lock",27:"Esc",32:"Space",33:"Page Up",34:"Page Down",35:"End",36:"Home",37:"Left",38:"Up",39:"Right",40:"Down",45:"Insert",46:"Delete",
 		// 48:"0",49:"1",50:"2",51:"3",52:"4",53:"5",54:"6",55:"7",56:"8",57:"9",
-		65:"A",66:"B",67:"C",68:"D",69:"E",70:"F",71:"G",72:"H",73:"I",74:"J",75:"K",76:"L",77:"M",78:"N",79:"O",80:"P",81:"Q",82:"R",83:"S",84:"T",85:"U",86:"V",87:"W",88:"X",89:"Y",90:"Z"
+		65:"A",66:"B",67:"C",68:"D",69:"E",70:"F",71:"G",72:"H",73:"I",74:"J",75:"K",76:"L",77:"M",78:"N",79:"O",80:"P",81:"Q",82:"R",83:"S",84:"T",85:"U",86:"V",87:"W",88:"X",89:"Y",90:"Z",
+		188: "," 
 	};
 
 	var KEYCHARTOCODE = {
@@ -564,7 +565,7 @@ $(document).ready(function () {
 				var letterDivs = $('#lex p');
 
 				
-				if (typeof letter === "undefined") {
+				if (typeof letter === "undefined" ) {
 								    
 				   	// playSound(other);
 
@@ -652,7 +653,7 @@ $(document).ready(function () {
 						
 						console.log(letters);
 
-					} else if (typeof letter === "undefined") {
+					} else if (typeof letter === "undefined" && language=="hebrew") {
 
 						    return;
 
