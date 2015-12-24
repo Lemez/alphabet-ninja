a9a09bc839d3330b01ec2d038c489c0ee80bb6ed
Jonathan Walton <lemez9@gmail.com>
2015-12-24 11:28:40 +0530

    playing with fb like button trying to get it to work again and commented out other fb funcs

--- js/keysynth_copy.js
+++ js/keysynth_copy.js
@@ -101,50 +101,50 @@ $(document).ready(function () {
 		xhr.send(data);
     }
 
-    function PostImageToFacebook( authToken, filename, mimeType, imageData, message )
-{
-    // this is the multipart/form-data boundary we'll use
-    var boundary = '----ThisIsTheBoundary1234567890';
+//     function PostImageToFacebook( authToken, filename, mimeType, imageData, message )
+// {
+//     // this is the multipart/form-data boundary we'll use
+//     var boundary = '----ThisIsTheBoundary1234567890';
     
-    // let's encode our image file, which is contained in the var
-    var formData = '--' + boundary + '\r\n'
-    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
-    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
-    for ( var i = 0; i < imageData.length; ++i )
-    {
-        formData += String.fromCharCode( imageData[ i ] & 0xff );
-    }
-    formData += '\r\n';
-    formData += '--' + boundary + '\r\n';
-    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
-    formData += message + '\r\n'
-    formData += '--' + boundary + '--\r\n';
+//     // let's encode our image file, which is contained in the var
+//     var formData = '--' + boundary + '\r\n'
+//     formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
+//     formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
+//     for ( var i = 0; i < imageData.length; ++i )
+//     {
+//         formData += String.fromCharCode( imageData[ i ] & 0xff );
+//     }
+//     formData += '\r\n';
+//     formData += '--' + boundary + '\r\n';
+//     formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
+//     formData += message + '\r\n'
+//     formData += '--' + boundary + '--\r\n';
     
-    var xhr = new XMLHttpRequest();
-    xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
-    xhr.onload = xhr.onerror = function() {
-        console.log( xhr.responseText );
-    };
-    xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
-    xhr.sendAsBinary( formData );
-}
+//     var xhr = new XMLHttpRequest();
+//     xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
+//     xhr.onload = xhr.onerror = function() {
+//         console.log( xhr.responseText );
+//     };
+//     xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
+//     xhr.sendAsBinary( formData );
+// }
 
-    function postToFb(canvas){
+//     function postToFb(canvas){
 
-    	if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
-    		XMLHttpRequest.prototype.sendAsBinary = function(string) {
-        		var bytes = Array.prototype.map.call(string, function(c) {
-            	return c.charCodeAt(0) & 0xff;
-        		});
-        	this.send(new Uint8Array(bytes).buffer);
-    		};
-		}
+//     	if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
+//     		XMLHttpRequest.prototype.sendAsBinary = function(string) {
+//         		var bytes = Array.prototype.map.call(string, function(c) {
+//             	return c.charCodeAt(0) & 0xff;
+//         		});
+//         	this.send(new Uint8Array(bytes).buffer);
+//     		};
+// 		}
 
-    	var c = canvas.toDataURL('image/png');
-        var encodedPng = c.substring(c.indexOf(',')+1,c.length);
-        var decodedPng = atob(encodedPng);
-        PostImageToFacebook(response.authResponse.accessToken, 'AlphabetNinja.png', 'image/png', decodedPng, '');
-    }
+//     	var c = canvas.toDataURL('image/png');
+//         var encodedPng = c.substring(c.indexOf(',')+1,c.length);
+//         var decodedPng = atob(encodedPng);
+//         PostImageToFacebook(response.authResponse.accessToken, 'AlphabetNinja.png', 'image/png', decodedPng, '');
+//     }
 
 
 	 function download() {
